6896284fc656e1306919e2199a53c0228d317087
appatoredk <cosmofftiktok@gmail.com>
2026-09-12 00:28:06 -0600

    index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
+import { fetchApp } from "parse-play";
+
 export default {
   async fetch(request, env, ctx) {
 
-    const datos = {
-      name: "Aplicación de prueba",
-      appId: "com.prueba.app",
-      icon: "https://raw.githubusercontent.com/github/explore/main/topics/android/android.png",
-      details: "Esta es una aplicación de prueba funcionando con Cloudflare Workers."
-    };
+    try {
 
-    return new Response(
-      JSON.stringify(datos),
-      {
-        headers: {
-          "Content-Type": "application/json; charset=UTF-8"
+      const app = await fetchApp(
+        "com.google.android.youtube",
+        {
+          language: "ES",
+          country: "MX"
         }
-      }
-    );
+      );
+
+      return new Response(
+        JSON.stringify({
+          name: app?.name || "Sin nombre",
+          appId: app?.app_id || "com.google.android.youtube",
+          icon: app?.icon_url || "",
+          details: app?.description || ""
+        }),
+        {
+          headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+            "Access-Control-Allow-Origin": "*"
+          }
+        }
+      );
+
+    } catch (error) {
+
+      return new Response(
+        JSON.stringify({
+          error: true,
+          message: error.message
+        }),
+        {
+          status: 500,
+          headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+            "Access-Control-Allow-Origin": "*"
+          }
+        }
+      );
+    }
   }
 };
